ae0e5ee132800e115548147e77cb0f12adffe510
Benji Hooper <benji@sixp.in>
2018-12-17 19:11:10 +0000

    Move CSV parsing into upload-service, customer-contact is now defined in grpc protobuf

--- customer-contact-service/handlers/handlers.go
+++ customer-contact-service/handlers/handlers.go
@@ -2,9 +2,7 @@ package handlers
 
 import (
 	"context"
-	"encoding/csv"
 	"log"
-	"strings"
 
 	"github.com/benjih/grpc-services-test/customer-contact-service/controllers"
 	pb "github.com/benjih/grpc-services-test/grpc"
@@ -13,14 +11,7 @@ import (
 type Server struct{}
 
 func (s *Server) AddOrUpdateCustomerContact(ctx context.Context, in *pb.CustomerContactRequest) (*pb.CustomerContactReply, error) {
-	csvReader := csv.NewReader(strings.NewReader(in.Data))
-	record, err := csvReader.Read()
-	if err != nil {
-		log.Print(err.Error())
-		return &pb.CustomerContactReply{}, nil
-	}
-
-	if err := controllers.AddOrUpdateCustomerContact(record); err != nil {
+	if err := controllers.AddOrUpdateCustomerContact([]string{in.Id, in.Name, in.EmailAddress, in.TelephoneNumber}); err != nil {
 		log.Print(err.Error())
 		return &pb.CustomerContactReply{}, nil
 	}
--- upload-service/handlers/handlers.go
+++ upload-service/handlers/handlers.go
@@ -3,8 +3,10 @@ package handlers
 import (
 	"bufio"
 	"context"
+	"encoding/csv"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	pb "github.com/benjih/grpc-services-test/grpc"
@@ -25,7 +27,21 @@ func UploadCustomerContactsCSVHandler(consumerContactServiceUrl string) http.Han
 		for fscanner.Scan() {
 			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 			defer cancel()
-			_, err = client.AddOrUpdateCustomerContact(ctx, &pb.CustomerContactRequest{Data: fscanner.Text()})
+			text := fscanner.Text()
+			csvReader := csv.NewReader(strings.NewReader(text))
+			record, err := csvReader.Read()
+			if err != nil {
+				log.Print(err.Error())
+				w.WriteHeader(500)
+				return
+			}
+
+			_, err = client.AddOrUpdateCustomerContact(ctx, &pb.CustomerContactRequest{
+				Id:              record[0],
+				Name:            record[1],
+				EmailAddress:    record[2],
+				TelephoneNumber: record[3],
+			})
 			if err != nil {
 				log.Fatalf("Failed to update customer-contact-service: %v", err)
 			}
